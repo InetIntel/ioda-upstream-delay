f0371364eaf2cec995094132cdd2ae16250fc999
Tanya <tanyagarg1701@gmail.com>
2023-04-13 03:27:21 +0000

    Added main probing scripts

---
+++ probe.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+count = 0
+with open("probe_input.txt") as file:
+    for line in file:
+        count = 0
+        while count < 6:
+            os.system('python3 run.py -r 100 ' + line.split()[0])
+            count += 1 
+            time.sleep(1)
