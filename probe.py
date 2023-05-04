33cb182ab5bb659d37a0c90c5ab36191fb0e6731
Tanya <tanyagarg1701@gmail.com>
2023-05-04 12:41:34 +0000

    Updated output formatting; Added normal traceroute

--- probe.py
+++ probe.py
@@ -6,6 +6,7 @@ with open("probe_input.txt") as file:
     for line in file:
         count = 0
         while count < 6:
-            os.system('python3 run.py -r 100 ' + line.split()[0])
+            os.system('python3 run_json.py -r 100 ' + line.split()[0])
+            os.system('python3 run_trace.py' + line.split()[0])
             count += 1 
             time.sleep(1)
