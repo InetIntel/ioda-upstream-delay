42b3a7d216c45c43f4e007bd4b83a016a46536bc
Tanya Garg <tanya@Tanyas-MacBook-Air.local>
2023-10-10 19:27:07 -0400

    Corrected things

--- probe.py
+++ probe.py
@@ -5,7 +5,7 @@ count = 0
 with open("probe_input.txt") as file:
     while count < 10:
         for line in file:
-            os.system('python3 run_json.py -r 100 ' + line.split()[0] + " " + count)
+            os.system('python3 run_json.py -r 100 ' + line.split()[0] + " " + str(count))
             print("Done")
             time.sleep(1)
         count += 1
