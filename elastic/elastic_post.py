fb0dd3417e318144ac9b3b11937146f55970f118
Tanya Garg <tanya@Tanyas-MacBook-Air.local>
2023-12-05 14:39:19 -0500

    Added elastic post

--- elastic/elastic_post.py
+++ elastic/elastic_post.py
@@ -1,30 +1,57 @@
 import json
 import requests
 
-elasticsearch_url = 'http://localhost:9200'  # Replace with the correct URL
-index_name = 'yarrp'  # Replace with the desired index name
-document_id = ""  # Replace with the desired document ID (or omit for auto-generation)
-file = ""
+def post_elastic(file_path):
 
-# Read the JSON data from your file
-with open(file, 'r') as json_file:
-    json_data = json.load(json_file)
+    elasticsearch_url = 'https://localhost:9200'  # Replace with the correct URL
+    index_name = 'yarrp'  # Replace with the desired index name
+    document_id = file_path.split(".")[0][:-1]  # Replace with the desired document ID (or omit for auto-generation)
+    # file_path = ""  # Replace with the path to your JSON file
+    username = 'elastic'  # Replace with your Elasticsearch username
+    password = 'NXGQfMtUJzaF0k6FiY5K'  # Replace with your Elasticsearch password
+    ca_cert_path = '../../elasticsearch-8.10.2/config/certs/http_ca.crt'  # Replace with the path to your CA certificate
 
-# Create the Elasticsearch document URL
-if document_id:
-    url = f'{elasticsearch_url}/{index_name}/_doc/{document_id}'
-else:
-    url = f'{elasticsearch_url}/{index_name}/_doc'
+    # Read the JSON data from your file
+    with open(file_path, 'r') as json_file:
+        json_data = json.load(json_file)
 
-# Set the headers to specify that you're sending JSON data
-headers = {'Content-Type': 'application/json'}
+    # Create the Elasticsearch document URL
+    if document_id:
+        url = f'{elasticsearch_url}/{index_name}/_doc/{document_id}'
+    else:
+        url = f'{elasticsearch_url}/{index_name}/_doc'
 
-# Send a POST request to upload the JSON data to Elasticsearch
-response = requests.post(url, data=json.dumps(json_data), headers=headers)
+    # Set the headers to specify that you're sending JSON data and include authentication
+    headers = {'Content-Type': 'application/json'}
+    auth = (username, password)
 
-# Check the response from Elasticsearch
-if response.status_code == 201:
-    print("Data successfully uploaded to Elasticsearch.")
-else:
-    print(f"Failed to upload data. Response status code: {response.status_code}")
-    print(response.text)
+    # Send a POST request to upload the JSON data to Elasticsearch with authentication and SSL verification
+    response = requests.post(url, data=json.dumps(json_data), headers=headers, auth=auth, verify=ca_cert_path)
+
+    # Check the response from Elasticsearch
+    if response.status_code == 201:
+        print("Data successfully uploaded to Elasticsearch.")
+    else:
+        print(f"Failed to upload data. Response status code: {response.status_code}")
+        print(response.text)
+
+def delete_elastic_index():
+    # Define the Elasticsearch server URL and index name
+    elasticsearch_url = 'http://localhost:9200'  # Replace with the correct URL
+    index_name = 'yarrp'  # Replace with the index where you want to delete documents
+
+
+    # Create the Elasticsearch document URL
+    url = f'{elasticsearch_url}/{index_name}'
+
+    # Send a DELETE request to delete the document
+    response = requests.delete(url)
+
+    # Check the response from Elasticsearch
+    if response.status_code == 200:
+        print(f"Index '{index_name}' deleted successfully.")
+    elif response.status_code == 404:
+        print(f"Index '{index_name}' not found.")
+    else:
+        print(f"Failed to delete the index. Response status code: {response.status_code}")
+        print(response.text)
